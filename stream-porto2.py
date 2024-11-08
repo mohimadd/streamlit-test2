328947d72f95be779d6ad1e1ddd255d1987a2d54
mohimadd <m.imadudin68@gmail.com>
2024-11-08 15:17:33 +0700

    Update stream-porto2.py

--- stream-porto2.py
+++ stream-porto2.py
@@ -21,7 +21,7 @@ with right:
     customer_rating = st.number_input('input customer rating')
 
 with left:
-    product_priority = st.number_input('input product priority')
+    product_priority = st.number_input('input product prior before')
 
 with right:
     discount_offerd = st.number_input('input discount offered')
